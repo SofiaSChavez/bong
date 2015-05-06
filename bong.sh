41dc0b652e7b186bacbc8534810e535f2f1b8b42
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2015-05-06 16:37:02 +0200

    0   0                 |   |             ____|___|____          0  |~ ~ ~ ~ ~ ~|   0          |  |           |   |       ___|__|___________|___|__       |/\/\/\/\/\/\/\/\/\/\/\/|   0   |       H a p p y       |   0   |   |/\/\/\/\/\/\/\/\/\/\/\/|   |  _|___|_______________________|___|__ |/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/| |                                   | |         B i r t h d a y! ! !      | | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ | |___________________________________|

--- bong.sh
+++ bong.sh
@@ -6,13 +6,47 @@ paddleLeft=50
 scoreLeft=0
 scoreRight=0
 paddleRight=50
-ballx=50
-bally=50
-speedx=1
-speedy=1
+ballX=50
+ballY=50
+speedX=1
+speedY=1
+
+width=50
+height=50
+
+top=0
+bottom=49
+left=0
+right=49
+
+gameOver=0
+winner=0
 
 update() {
-	echo hi
+	# Test whether ball collides with wall
+	if [ $ballX -eq $left ]; then
+		speedX=1
+	#else if [ $ballX -eq $right ]; then
+		speedX=-1
+	fi
+	
+	if [ $ballY -eq $top ]; then
+		speedY=1
+	elif [ $ballY -eq $bottom ]; then
+		speedY=-1
+	fi
+
+	# Test whether Player 1 loses
+	if [ $ballX -eq $left ]; then
+		gameOver=1
+		winner=1
+	elif [ $ballX -eq $width ]; then
+		gameOver=1
+		winner=0
+	fi
+	
+	let ballX=ballX+speedX
+	let ballY=ballY+speedY
 }
 
 render() {
