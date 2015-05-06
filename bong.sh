4ca8f46fa171d1eea6f421f286d09cdec11bfe2a
codedust <codedust@users.noreply.github.com>
2015-05-06 15:59:43 +0200

    Add :cake:

--- bong.sh
+++ bong.sh
@@ -1,4 +1,30 @@
 tput clear
 tput cup 2 2
 echo X
-read
+
+paddleLeft=50
+scoreLeft=0
+scoreRight=0
+paddleRight=50
+ballx=50
+bally=50
+speedx=1
+speedy=1
+
+update() {
+	echo hi
+}
+
+render() {
+	echo ho
+}
+
+main() {
+	while(true); do
+		update
+		render
+		sleep 0.1
+	done;
+}
+
+main
